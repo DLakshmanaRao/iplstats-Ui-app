9e341b5bf2103a1328b4d02bb4e384dba29c54b3
LakshmanaRao <ldevadhar@gmail.com>
2023-04-22 19:17:53 +0530

    added register options

---
+++ register.js
@@ -0,0 +1,40 @@
+
+const register = document.getElementById("register");
+let message = document.getElementById("message");
+message.style.visibility = 'hidden';
+register.onclick = function register() {
+  console.log("Login")
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const mail = document.getElementById("gmail").value;
+  var myHeaders = new Headers();
+
+  if(user != null && password !=null){
+    var raw = JSON.stringify({
+      "username": username,
+      "password": password,
+      "email": mail
+    });
+  }else {
+    message.style.visibility = 'visible';
+    message.style.color = 'red';
+    message.innerHTML = "Entered credentials are not valid";
+  }
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw
+  };
+
+  fetch("https://iplstatsapp.onrender.com/iplstats/api/auth/register", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      localStorage.setItem('token', result.jwt);
+      console.log(result)
+    }
+    )
+    .catch(error => console.log('error', error));
+}
