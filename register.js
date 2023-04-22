c950dce0b049062e9769a552836094210fc81790
LakshmanaRao <ldevadhar@gmail.com>
2023-04-22 19:59:24 +0530

    added changes in regester.js

--- register.js
+++ register.js
@@ -1,40 +1,40 @@
 
 const register = document.getElementById("register");
 let message = document.getElementById("message");
+let message2 = document.getElementById("message2");
 message.style.visibility = 'hidden';
 register.onclick = function register() {
   console.log("Login")
   const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value; 
+  const password = document.getElementById("password").value;
   const mail = document.getElementById("gmail").value;
   var myHeaders = new Headers();
-  var raw ={};
-  if(user != null && password !=null){
+  if (user != null && password != null) {
     raw = JSON.stringify({
       "username": username,
       "password": password,
-      "email": mail
+      "email": mail + 'gmail.com'
     });
-  }else {
+
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw
+    };
+
+    fetch("https://iplstatsapp.onrender.com/iplstats/api/auth/register", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        message2.innerHTML = `User created with username : ${username}`;
+      })
+      .catch(error => console.log('error', error));
+  } else {
     message.style.visibility = 'visible';
     message.style.color = 'red';
     message.innerHTML = "Entered credentials are not valid";
   }
-  myHeaders.append("Content-Type", "application/json");
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw
-  };
-
-  fetch("https://iplstatsapp.onrender.com/iplstats/api/auth/register", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      localStorage.setItem('token', result.jwt);
-      console.log(result)
-    }
-    )
-    .catch(error => console.log('error', error));
 }
